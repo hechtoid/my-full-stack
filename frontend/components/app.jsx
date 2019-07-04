598c306b9275c822fea1fbb920d08cbdad27e1f3
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-07-04 16:00:22 -0700

    mostly auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
 import Banner from './banner/banner_container'
+import SignUpFormContainer from './session_form/signup_form_container';
+import SignInFormContainer from './session_form/signin_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 
 const App = () => (
     <div>
         <header>
-        <h1>nurdCamp</h1>
+            <Link to="/" className="header-link">
+                <h1>nurdCamp</h1>
+            </Link>
         <Banner />
         </header>
+        <Switch>
+            <AuthRoute exact path="/signin" component={SignInFormContainer} />
+            <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+            <Route exact path="/" to="/" />
+        </Switch>
+
     </div>
 );
 
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -9,7 +9,20 @@ import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () =>{
     const root = document.getElementById("root")
-    const store = configureStore()
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { currentUser: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
     ReactDOM.render(<Root store={store} />, root)
     
     window.store = store
