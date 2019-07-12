eb16668872bfee1b680f96d0168d163c317278a7
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-07-12 10:58:25 -0700

    song links

--- frontend/components/song/song.jsx
+++ frontend/components/song/song.jsx
@@ -1,20 +1,74 @@
 import React from 'react';
 import { withRouter, Redirect, Link } from 'react-router-dom';
 
+class Song extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            artist: 'RadioHead',
+            title: 'These are my Twisted Words',
+            // lyricslink: ''
+        }
+    }
 
-const Song = () => {
-   document.title = "the Song"
-    return(
+    componentDidMount() {
+        document.title = "the Song"
+        this.setState({
+lyricsLink: 
+`http://www.google.com/search?q=${this.state.artist} 
+lyrics "${this.state.title}"`,
+whosampledLink: 
+`https://www.whosampled.com/search/?q=${this.state.artist} 
+${this.state.title}`,
+spotiLink: 
+`https://open.spotify.com/search/results/${this.state.artist} 
+${this.state.title}`
+        })
+
+    }
+    
+    render(){ 
+    
+        return(
  
-    <div className="song-player">
-
+    <div className="song-show">
         <audio controls
         src='radiohead-these_are_my_twisted_words-(advance)-2009-woi/01-radiohead-these_are_my_twisted_words-(advance)-2009-woi.mp3'>
-
         </audio>
-
+    <div className="song-info">
+            <a 
+            className="song-links" 
+            href={this.state.lyricsLink} 
+            target="_blank">
+        <div className="song-links-div" >
+                Search for Lyrics
+        </div>
+            </a>  
+        
+            <a 
+            className="song-links" 
+            href={this.state.spotiLink} 
+            target="_blank">
+        <div className="song-links-div" >
+                check Spotify 
+        </div>
+            </a>  
+       
+            <a 
+            className="song-links" 
+            href={this.state.whosampledLink} 
+            target="_blank">
+        <div className="song-links-div" >
+                Search whoSampled
+        </div>
+            </a>  
+        
+        
+        
+    </div>
+    
 
     </div>
 )}
-
+}
 export default Song
