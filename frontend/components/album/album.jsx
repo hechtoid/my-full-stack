db88ebc0575dd0ef3c5847df5c38facf1b11a04e
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-10-28 15:02:58 -0700

    slightly better album show

--- frontend/components/album/album.jsx
+++ frontend/components/album/album.jsx
@@ -10,17 +10,18 @@ componentDidMount(){
 
 render(){
 return(
-    <Link to="/albums/1/artists/">
+    <div className="album">
     <div className="album-show">
     <Redirect to="/albums/1" />
         <div className="album-title">the Dark Side of the Moon</div>
         <em>the only album on the internet</em>
+    <Link to="/albums/1/artists/">
         <div className="album-art-div">
-       
         <img className="album-art" src='dsotm.jpg' title='the only album on the internet'></img>
         </div>
-    </div>
     </Link>
+    </div>
+    </div>
 )
 }
 }
